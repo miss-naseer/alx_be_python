a223fbc098c68d7eb1740f54abe686798c9326d4
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-05-24 00:08:07 +0100

    added operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,8 @@
-ECHO is on.
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number1 - number2 
+product = number1 * number2
+print("Addition of 10 and 5 is", sum)
+print("Subtraction of 10 and 5 is", sub)
+print("Multiplication of 10 and 5 is", product)
