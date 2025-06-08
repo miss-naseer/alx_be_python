324b806427ffb1328bddc8b1c5a8c750128d48af
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-06-08 01:02:29 +0100

    arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+# arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return "Error: Division by zero"
+            return num1 / num2
+        case _:
+            return "Invalid operation"
