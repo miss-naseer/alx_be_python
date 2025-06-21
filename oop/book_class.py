f967f80c98a2fdefe2a890d4659d63251c214b37
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-06-22 00:30:51 +0100

    book_class.py created

---
+++ oop/book_class.py
@@ -0,0 +1,31 @@
+# book_class.py
+
+class Book:
+    def __init__(self, title, author, year):
+        """
+        Constructor method.
+        This method is automatically called when you create a new instance.
+        """
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """
+        Destructor method.
+        Automatically called when an object is deleted or goes out of scope.
+        """
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """
+        User-friendly string representation (when you use print()).
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """
+        Developer/debug representation (used by repr()).
+        Should ideally allow recreating the object.
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
