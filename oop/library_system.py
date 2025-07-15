2f8683c66a25cb039b683dd303c7a294c0e1790f
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-07-15 12:48:16 +0100

    modified library_system

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,31 +1,31 @@
 class Book:
-    def _init_(self, title, author):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def _str_(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
-    def _init_(self, title, author, file_size):
-        super()._init_(title, author)
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
         self.file_size = file_size  # in KB
 
-    def _str_(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
-    def _init_(self, title, author, page_count):
-        super()._init_(title, author)
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
         self.page_count = page_count
 
-    def _str_(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
-    def _init_(self):
+    def __init__(self):
         self.books = []
 
     def add_book(self, book):
